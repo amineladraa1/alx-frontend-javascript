02fc8f0fc59a5bd42c8efa07f1e5955562205f59
amineladraa1 <amineladraa67@gmail.com>
2024-03-06 06:37:22 -0800

    all

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,11 @@
-import { uploadPhoto, createUser } from "./utils.js";
+import { createUser, uploadPhoto } from './utils';
 
 function handleProfileSignup() {
-    Promise.all([uploadPhoto(), createUser()])
-        .then(results => {
-            console.log(`${results[0].body} ${results[1].firstName} ${results[0].lastName}`);
-        })
-        .catch(() => console.log('Signup system offline'));
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((data) => {
+      console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
 }
 
 export default handleProfileSignup;
